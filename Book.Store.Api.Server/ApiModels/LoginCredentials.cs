5e12f4a22ed6e841f7a72743be9c9e42cb0d4daf
Peter Ayobami <33873999+peterayobami@users.noreply.github.com>
2024-12-12 15:58:12 +0100

    login credentials comments

--- Book.Store.Api.Server/ApiModels/LoginCredentials.cs
+++ Book.Store.Api.Server/ApiModels/LoginCredentials.cs
@@ -2,7 +2,14 @@ namespace Book.Store.Api.Server
 {
     public class LoginCredentials
     {
+        /// <summary>
+        /// The user's email
+        /// </summary>
         public string Email { get; set; }
+
+        /// <summary>
+        /// The user's password
+        /// </summary>
         public string Password { get; set; }
     }
 }
